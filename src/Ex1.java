7e51ec0e2b2bfe3a473fd272768363dd46759faf
TovarnyhViktor <milasalvska2@gmail.com>
2017-09-14 10:21:31 +0300

    Законченное 1 задание

--- src/Ex1.java
+++ src/Ex1.java
@@ -10,6 +10,13 @@ public class Ex1 {
         for (int x = 0; x < mass; x++) {
             chisla[x] = input.nextInt();
         }
+        int five = 0;
+
+        for (int i = 0; i<chisla.length;i++){
+            if (chisla[i]==5){
+                five++;
+            }
+        }
         int max = Integer.MIN_VALUE;
         for (int y = 0; y < chisla.length; y++) {
             max = Math.max(max, chisla[y]);
@@ -20,10 +27,18 @@ public class Ex1 {
         }
         System.out.println("Максимальный элемент массива: " + max);
         System.out.println("Минимальный элемент массива: " + min);
-        System.out.print ("Элементы массива :");
-        for (int i = 0; i < mass; i++) {
-            System.out.print (" " + chisla[i]);
+        System.out.println("Число 5 повторялось "+five+" раз");
+        for(int j=0;j<chisla.length;j++){
+            for(int i=0;i<chisla.length -1;i++){
+                if (chisla[i] > chisla[i+1]){
+                    int temp=chisla[i];
+                    chisla[i]=chisla[i+1];
+                    chisla[i+1]=temp;
+                }
+            }
         }
-        System.out.println();
+        for(int i=0; i<chisla.length;i++){
+            System.out.println("№"+i+"="+chisla[i]);
         }
     }
+}
